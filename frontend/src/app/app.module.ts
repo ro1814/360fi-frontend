c7cb16f8452ba060eb2d0684516f3888a8d4e939
ro1814 <rnsm14@gmail.com>
2022-01-12 11:25:41 +0100

    fundamental compoments done. Header module imported to app.module (linked and working)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { MainPageComponent } from './core/components/main-page/main-page.component';
+import { HeaderModule } from './core/components/headers/headers.module';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    MainPageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeaderModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
