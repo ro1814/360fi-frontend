27853f74ed47c82578a30eb1a76408dfbd373398
Ro1814 <rnsm14@gmail.com>
2022-10-04 22:18:41 +0200

    First div done with images and text

--- frontend/src/screens/ContactUsScreen.js
+++ frontend/src/screens/ContactUsScreen.js
@@ -7,11 +7,19 @@ const ContactUsScreen = () => {
       <div className="firstMeetUsDiv">
 
         <div className="firstSubDivMu">
-            
+
+            <div className="logoMUDiv">
               <img src="/logo-header360.svg" alt="360-logo" className="logoMU" />
+            </div>
 
             <div className="secondSubDivMu">
                 <div className="ukImageDiv">
+              <img src="/images/uk-map.svg" alt="ukMap" className="ukMap" />
+              <p>UNITED KINGDOM</p>
+              <p>Spaces Ealing Aurora 71-75</p>
+              <p>Uxbridge Road - Ealing</p>
+              <p>LONDON | W5 5SL</p>
+              <p>+44(0)795 714 7852</p>
                 </div>
                 <div className="spImageDiv">
                     
