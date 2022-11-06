58b1f787a4370438a08ccd9eb0ddca52da7da553
Ro1814 <rnsm14@gmail.com>
2022-11-06 21:47:38 +0100

    Login Register view almost done, not styled in mobile first. Modals with terms and conditions must be done

--- frontend/src/screens/LoginRegisterScreen.js
+++ frontend/src/screens/LoginRegisterScreen.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+import {} from "../styles/LoginRegisterScreenStyles.css";
+import { Form, Button, Modal } from "react-bootstrap";
 
 const LoginRegisterScreen = () => {
-    return (
-        <div>
-            <h1>Hi, Login page HERE!</h1>
+  return (
+    <div className="lRDiv">
+      <div className="leftLrDiv">
+        <div className="fFimgDiv">
+          <img
+            src="/images/logos/fund-Fairway-.png"
+            alt="FundFairway-logo"
+            className="fundFimg"
+          />
         </div>
-    )
-}
+        <div className="lRparagraphDiv">
+          <p className="lRparagraph">
+            Register for a free trial today and see how our powerful tools can
+            help you improve your business profitability.
+          </p>
+        </div>
+        <div className="lRbuttonsDiv">
+          <Button size="lg" className="fTbutton pill">
+            Free Trial
+          </Button>
+          <Button size="lg" className="loginButton pill">
+            Login
+          </Button>
+        </div>
+      </div>
+      <div className="rightLrDiv">
+        <img
+          src="/images/login-images.png"
+          alt="login-images"
+          className="loginImages"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default LoginRegisterScreen
+export default LoginRegisterScreen;
